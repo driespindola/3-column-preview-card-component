76e0d3a95738073c1924a8ad3a3c3764008872fd
Dri Espindola <103765212+driespindola@users.noreply.github.com>
2023-02-22 11:25:26 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Frontend Mentor | Stats preview card component</title>
+        <title>Frontend Mentor | 3-column preview card component</title>
         <link rel="shortcut icon" href="favicon-32x32.png" />
       </Head>
       <Component {...pageProps} />
